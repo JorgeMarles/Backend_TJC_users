01c46b188f6bb61ba221c32d7ada0aa1d61ac514
AlexaVJ <alexandra19vela@gmail.com>
2024-09-08 18:20:12 -0500

    Conexion to database

--- database/index.ts
+++ database/index.ts
@@ -1,14 +1,16 @@
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "../config";
-import { DataSource } from 'typeorm'
+import { DataSource } from 'typeorm';
 import { User } from "./models/User";
+import mysql from "mysql2";
 
 export const AppDataSource = new DataSource({
-  type: 'mysql',
+  type: 'mysql', 
+  driver: mysql, 
   host: DB_HOST,
   port: parseInt(DB_PORT),
   username: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
   synchronize: true,
-  entities: [User]
-})
+  entities: [User],
+});
