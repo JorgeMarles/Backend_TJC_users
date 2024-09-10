77936a556e70cc64f213ed3f1a18c02755d40208
Carlos Salazar <carlosalbertosame@ufps.edu.co>
2024-09-10 18:46:32 -0500

    BUILD Se agrego el dockerfile

--- dist/config/index.js
+++ dist/config/index.js
@@ -3,6 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.URL_FRONTEND = exports.TABLE_NAMES = exports.DB_PORT = exports.DB_NAME = exports.DB_HOST = exports.DB_PASSWORD = exports.DB_USER = exports.PORT = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-console.log(process.env);
+exports.PORT = process.env.PORT || "8080";
+exports.DB_USER = process.env.DB_USER || "";
+exports.DB_PASSWORD = process.env.DB_PASSWORD || "";
+exports.DB_HOST = process.env.DB_HOST || "";
+exports.DB_NAME = process.env.DB_NAME || "";
+exports.DB_PORT = process.env.DB_PORT || "";
+exports.TABLE_NAMES = process.env.TABLE_NAMES || "";
+exports.URL_FRONTEND = process.env.URL_FRONTEND || "";
--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,47 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+require("reflect-metadata");
+const config_1 = require("./config");
+const database_1 = require("./database");
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const UserRouter_1 = require("./routers/UserRouter");
+const ProductRouter_1 = require("./routers/ProductRouter");
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: config_1.URL_FRONTEND
+}));
+app.use(body_parser_1.default.json());
 app.use((req, res, next) => {
-    console.log("Middleware One");
+    console.log("Middleware");
     next();
 });
-app.use((req, res, next) => {
-    console.log("Middleware Two");
-    res.send("<h1>Response of the second middleware</h1>");
+app.use("/user", UserRouter_1.userRouter);
+app.use("/product", ProductRouter_1.productRouter);
+const run = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield database_1.AppDataSource.initialize();
+    }
+    catch (e) {
+        if (e instanceof Error) {
+            console.log(e.message);
+        }
+        else
+            console.log("Error during Data Source initialization");
+    }
+    app.listen(config_1.PORT, () => console.log(`Listening in port ${config_1.PORT}`));
 });
-app.listen(3000, () => console.log("Listening in port 3000"));
+run();
