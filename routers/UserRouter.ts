f8811bb113d45635eb0e85388d3741116952a4ef
AlexaVJ <alexandra19vela@gmail.com>
2024-12-08 10:20:13 -0500

    feat: add permission based on roles

--- routers/UserRouter.ts
+++ routers/UserRouter.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { create, disable, find, update } from "../controllers/UserController";
+import { authenticate } from "../services/SessionService";
 
 export const userRouter = express.Router();
 
 userRouter.post("/", create);
-userRouter.delete("/", disable);
-userRouter.put("/", update);
-userRouter.get("/", find);
+userRouter.delete("/", authenticate(['admin']), disable);
+userRouter.put("/", authenticate(['admin']), update);
+userRouter.get("/", authenticate(['admin']), find);
 //userRouter.get("/all", get_all);
 //userRouter.get("/:id", );
--- services/SessionService.ts
+++ services/SessionService.ts
@@ -2,28 +2,41 @@ import { NextFunction, Request, Response } from "express"
 import { UserRepository } from "../repositories/UserRepository";
 import { User } from "../database/entity/User";
 import jwt from "jsonwebtoken";
-import crypto  from "crypto"
 import bcrypt from "bcrypt";
+import fs from 'fs';
 
-const SECRET_KEY = crypto.randomBytes(64).toString('hex');
+// const SECRET_KEY = crypto.randomBytes(64).toString('hex');
+const PRIVATE_KEY = fs.readFileSync("./private.key", "utf8");
+const PUBLIC_KEY = fs.readFileSync("../public.key", "utf8");
 
 interface Credentials {
     email: string;
     password: string;
 };
 
+interface TokenPayload {
+    email: string;
+    type: string;
+    exp: number;
+}
+
+interface CustomRequest extends Request {
+    user?: { email: string; type: string };
+  }
+
 export const login = async (req: Request, res: Response) => {
-    try {        
-        const credentials: Credentials  = req.body;
+    try {
+        const credentials: Credentials = req.body;
         const user: unknown = await UserRepository.findOneBy({ email: credentials.email });
         if (user instanceof User) {
             const passwordMatched = await bcrypt.compare(credentials.password, user.password);
             if (passwordMatched) {
-                const token = jwt.sign( { email: credentials.email, type: user.type } , SECRET_KEY, { expiresIn: "1h" });
+                const type = user.type ? "admin" : "user";
+                const token = jwt.sign({ email: credentials.email, type: type }, PRIVATE_KEY, { algorithm: "RS256", expiresIn: "1h" });
                 return res.status(200).json({ token });
             }
             else throw Error("The password is incorrect");
-        } 
+        }
         else throw Error("The user don't exists");
     }
     catch (error: unknown) {
@@ -32,21 +45,33 @@ export const login = async (req: Request, res: Response) => {
             return res.status(400).send({ message: error.message });
         }
         else {
-            return res.status(400).send({ message: "Something went wrong"});
+            return res.status(400).send({ message: "Something went wrong" });
         }
     }
 };
 
-export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
-    const header = req.header("Authorization") || "";
-    const token = header.split(" ")[1];
-    if (!token) {
-      return res.status(401).json({ message: "Token not provied" });
-    }
-    try {
-      const payload = jwt.verify(token, SECRET_KEY);
-      next();
-    } catch (error) {
-      return res.status(403).json({ message: "Token not valid" });
-    }
-}; 
+export const authenticate = (roles: string[]) => {
+    return async (req: CustomRequest, res: Response, next: NextFunction) => {
+      const header = req.header("Authorization");
+      if (!header) {
+        return res.status(401).json({ message: "Authorization header missing" });
+      }
+  
+      const token = header.split(" ")[1];
+      if (!token) {
+        return res.status(401).json({ message: "Token not provided" });
+      }
+  
+      try {
+        const payload = jwt.verify(token, PUBLIC_KEY, { algorithms: ["RS256"] }) as TokenPayload;
+        req.user = payload;
+        if (!roles.includes(payload.type)) {
+          return res.status(403).json({ message: "Access denied" });
+        }
+  
+        next();
+      } catch (error) {
+        return res.status(403).json({ message: "Invalid or expired token" });
+      }
+    };
+  };
