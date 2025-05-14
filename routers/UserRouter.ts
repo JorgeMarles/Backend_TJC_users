e47dfd865a4fa975156b3da7e88f7f4bb28ca988
JorgeMarles <jamarlesf@gmail.com>
2025-05-13 23:12:47 -0500

    fix: let users find users

--- routers/UserRouter.ts
+++ routers/UserRouter.ts
@@ -11,4 +11,4 @@ userRouter.put("/recoveryPassword", updatePassword);
 userRouter.delete("/", authenticate(['admin']), disable);
 userRouter.put("/", authenticate(['admin', 'user']), update);
 userRouter.get("/findOne", authenticate(['admin', 'user', 'service']), findOne);
-userRouter.get("/", authenticate(['admin']), find);
+userRouter.get("/", authenticate(['admin', 'user']), find);
